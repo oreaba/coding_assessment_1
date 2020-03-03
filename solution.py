871c8d4a3c85bc527a7f704d4f90e2acee3ab737
Paul Odera <oderapaul7@gmail.com>
2020-03-03 11:09:08 +0300

    comments, test fixes and task 3 test set up

--- solution.py
+++ solution.py
@@ -1,8 +1,10 @@
 def exercise1(person, people):
     affiliated_teams = []
     for p in people:
-        if p.is_team:
+        if p.is_team: # we are only interested in checking teams that the individual belongs
             for m in p.members:
+                # teams could have nested members
+                # i.e. a team could be a sub-set of another team
                 if m.is_team:
                     sub_team = [s_t.id for s_t in m.members]
                     if person.id in (sub_team):
@@ -13,3 +15,6 @@ def exercise1(person, people):
     
     return affiliated_teams
 
+
+def get_members(team):
+    
--- tests.py
+++ tests.py
@@ -4,16 +4,22 @@ import data1
 import data2
 import data3
 
+
 class TestPerson(unittest.TestCase):
-    def test_affiliation_1(self):
+    def test_affiliated_teams1(self):
         self.assertEqual([t.displayname for t in solution.exercise1(data1.alice, data1.people)], ['The A-Team', 'The C-Team'])
     
-    def test_affiliation_2(self):
-        self.assertEqual([t.displayname for t in solution.exercise1(data1.alice, data1.people)], ['The A-Team', 'The C-Team'])
+    def test_affiliated_teams2(self):
+        self.assertEqual([t.displayname for t in solution.exercise1(data2.alice, data2.people)], ['The A-Team', 'The C-Team'])
     
-    def test_affiliation_3(self):
+    def test_affiliated_teams3(self):
         self.assertEqual([t.displayname for t in solution.exercise1(data2.bob, data2.people)], ['The A-Team','The B-Team', 'The C-Team'])
 
+    def test_affiliated_members1(self):
+        self.assertEqual(sorted(p.displayname for p in solution.get_members(data2.c_team)), ['Alice', 'Bob', 'Carlos', 'Charlie', 'Eve'])
+    
+    def test_affiliated_members2(self):
+        self.assertEqual(sorted(p.displayname for p in solution.get_members(data3.c_team)), ['Alice', 'Bob', 'Carlos', 'Charlie', 'Eve', 'Peggy', 'Trent', 'Victor'])
 
 
 if __name__ == '__main__':
